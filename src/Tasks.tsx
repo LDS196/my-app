84d2e7b4d0cf5cf4f1c036a04e29f3c012d5c592
LDS196 <lihachevd87@gmail.com>
2022-12-06 09:41:51 +0400

    new component

---
+++ src/Tasks.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+type TasksPropsType = {
+    data: {
+        title: string,
+        tasks: TaskType[],
+        students: string[],
+    }
+}
+type TaskType={
+    tasksId?: number,
+    title: string,
+    isDone: boolean
+}
+
+export const Tasks = (props: TasksPropsType) => {
+    let tasks = props.data.tasks.map( el =>  <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li> )
+    let students = props.data.students.map(student => <p>{student}</p>)
+    return (
+        <div>
+            <h3>{props.data.title}</h3>
+
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {tasks}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+            {students}
+        </div>
+    )
+}
